e4d85f86ceb206397b84a037d3b471c64013c63c
Maxentr <rouaultmaxent@gmail.com>
2021-03-27 12:06:17 +0100

    change port to patch heroku H10 bug

--- app.js
+++ app.js
@@ -260,6 +260,6 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(8000, () => {
-    console.log(`listening on port 8000`);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
