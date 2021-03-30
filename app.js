6321742f5e58523af4dc6faa90dbf1e277894e92
Maxent <67283154+Maxentr@users.noreply.github.com>
2021-03-30 16:09:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+server.listen(app.get('port'));
 
 //ROUTING
 app.get('/', (req, res) => {
