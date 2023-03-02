99a186dafd569ff3f062f379c4865d425aafadb9
maxentr <rouaultmaxent@gmail.com>
2023-03-02 17:48:30 +0100

    feat(ui): add button component

--- packages/workshop/stories/Button.stories.tsx
+++ packages/workshop/stories/Button.stories.tsx
@@ -1,20 +1,22 @@
 import React from "react"
 import { Meta, Story } from "@storybook/react"
-import { Button } from "ui-web"
+import { Button } from "ui"
 
 const meta: Meta = {
   title: "Button",
   component: Button,
-  argTypes: {
-    label: { control: "text" },
-  },
 }
 
-const Template: Story = (args) => <Button label={args.label} {...args} />
+const Template: Story = (args) => (
+  <Button label={args.label} variant={args.variant} color={args.color} size={args.size} loading={args.loading} />
+)
 
 export const Primary = Template.bind({})
 Primary.args = {
   label: "Button",
+  variant: "filled",
+  color: "primary",
+  size: "normal",
 }
 
 export default meta
