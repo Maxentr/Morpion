b635c3e65e2aea693aa34eecb532e894fea70c22
maxentr <rouaultmaxent@gmail.com>
2023-03-02 17:46:48 +0100

    build(tailwind-config): add custom colors

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./stories/**/*.{js,ts,jsx,tsx}",
     // include packages if not transpiling
-    "../../packages/ui-web/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fffffe",
+        secondary: "#f45d48",
+        highlight: "#078080",
+        tertiary: "#f8f5f2",
+        customBlack: "#232323",
+      },
+    },
+  },
   plugins: [],
-};
+}
