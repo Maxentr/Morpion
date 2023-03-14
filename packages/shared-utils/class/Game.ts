36235421c6dbdd11ecc9a33ac6d2ff91fcb88fe8
maxentr <rouaultmaxent@gmail.com>
2023-03-14 21:20:36 +0100

    refactor(shared-utils): add toJSON function to Game. Use of prefixes

--- packages/shared-utils/class/Game.ts
+++ packages/shared-utils/class/Game.ts
@@ -2,7 +2,11 @@ import { Player } from "./Player"
 
 export type GAME_STATUS = "lobby" | "playing" | "finished"
 
-export abstract class Game<T extends Player = Player> {
+export type GameToJSON<T extends Player = Player> = Pick<
+  IGame<T>,
+  "id" | "status" | "maxPlayers" | "players" | "turn" | "private"
+>
+export interface IGame<T extends Player = Player> {
   id: string
   status: GAME_STATUS
   maxPlayers: number
@@ -10,40 +14,94 @@ export abstract class Game<T extends Player = Player> {
   turn: number
   private: boolean
 
+  getPlayer(playerSocketID: string): T | undefined
+  addPlayer(player: T): void
+  removePlayer(playerSocketID: string): void
+  isFull(): boolean
+  start(): void
+  checkTurn(playerSocketID: string): boolean
+  nextTurn(): void
+  reset(): void
+  toJSON(): GameToJSON<T>
+}
+
+export abstract class Game<T extends Player = Player> implements IGame<T> {
+  // generate a random game id with 8 characters (a-z, A-Z, 0-9)
+  private _id: string = Math.random().toString(36).substring(2, 10)
+  private _status: GAME_STATUS = "lobby"
+  private _maxPlayers: number = 0
+  private _players: T[] = []
+  private _turn: number = 0
+  private _private: boolean = false
+
   constructor(maxPlayers: number, privateGame: boolean) {
-    // generate a random game id with 8 characters (a-z, A-Z, 0-9)
-    this.id = Math.random().toString(36).substring(2, 10)
     this.maxPlayers = maxPlayers
-    this.status = "lobby"
-    this.players = []
-    this.turn = 0
     this.private = privateGame
 
     return this
   }
 
-  getPlayer(playerSocketID: string): T | undefined {
+  public get id(): string {
+    return this._id
+  }
+
+  public get status(): GAME_STATUS {
+    return this._status
+  }
+  public set status(value: GAME_STATUS) {
+    this._status = value
+  }
+
+  public get maxPlayers(): number {
+    return this._maxPlayers
+  }
+  private set maxPlayers(value: number) {
+    this._maxPlayers = value
+  }
+
+  public get players(): T[] {
+    return this._players
+  }
+  private set players(value: T[]) {
+    this._players = value
+  }
+
+  public get turn(): number {
+    return this._turn
+  }
+  private set turn(value: number) {
+    this._turn = value
+  }
+  
+  public get private(): boolean {
+    return this._private
+  }
+  private set private(value: boolean) {
+    this._private = value
+  }
+
+  getPlayer(playerSocketID: string) {
     return this.players.find((player) => {
       return player.socketID === playerSocketID
     })
   }
 
-  addPlayer(player: T): void {
+  addPlayer(player: T) {
     if (this.isFull()) return
     this.players.push(player)
   }
 
-  removePlayer(playerSocketID: string): void {
+  removePlayer(playerSocketID: string) {
     this.players = this.players.filter((player) => {
       return player.socketID !== playerSocketID
     })
   }
 
-  isFull(): boolean {
+  isFull() {
     return this.players.length === this.maxPlayers
   }
 
-  start(): void {
+  start() {
     if (!this.isFull()) return
     this.status = "playing"
     this.turn = Math.floor(Math.random() * this.players.length)
@@ -51,16 +109,27 @@ export abstract class Game<T extends Player = Player> {
     console.log(`Game ${this.id} started`)
   }
 
-  checkTurn(playerSocketID: string): boolean {
+  checkTurn(playerSocketID: string) {
     return this.players[this.turn].socketID === playerSocketID
   }
 
-  nextTurn(): void {
+  nextTurn() {
     this.turn = (this.turn + 1) % this.players.length
   }
 
-  reset(): void {
+  reset() {
     this.status = "lobby"
     this.turn = 0
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      status: this.status,
+      maxPlayers: this.maxPlayers,
+      players: this.players,
+      turn: this.turn,
+      private: this.private,
+    }
   }
 }
