33860bddf90f1a6b5ee86bd5f0ab85f8725af5f5
maxentr <rouaultmaxent@gmail.com>
2023-04-23 16:42:45 +0200

    ci(tailwind-config): update path to includes every folders and sub-folders

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     // app content
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./stories/**/*.{js,ts,jsx,tsx}",
+    "./**/**/*.{js,ts,jsx,tsx}",
     // include packages if not transpiling
     "../../packages/ui/**/*.{js,ts,jsx,tsx}",
   ],
